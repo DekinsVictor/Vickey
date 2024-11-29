4c5d149f5bba3aaf0f5c58b06a7a4fd44cc6a716
DekinsVictor <victorakinyemi086@gmail.com>
2024-11-29 15:11:53 +0100

    I added Victor file
    I used the other github account file that is in my laptop file explorer

---
+++ victor/src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Victor Akinyemi | My Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'A Creative Visionary, A Digital Maverick in the Digital Marketing and Graphic Design World.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
